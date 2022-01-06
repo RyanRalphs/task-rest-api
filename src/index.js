1ba5a2ed4141568b47ce73b34df3b07763572124
Ryan <ryaaan_@live.co.uk>
2022-01-06 14:07:14 +0000

    Added update routes for user and task

--- src/index.js
+++ src/index.js
@@ -13,21 +13,9 @@ app.post('/users', async ({ body }, res) => {
     const user = new User(body)
     try {
         await user.save()
-         res.status(201).send('New User saved with name ' + user.name)
+        res.status(201).send('New User saved with name ' + user.name)
     } catch (error) {
-         res.status(400).send(error)
-    }
-})
-
-
-app.post('/tasks', async ({ body }, res) => {
-    const task = new Task(body)
-
-    try {
-        await task.save()
-         res.status(201).send('New Task saved with name ' + task.name)
-    } catch (error) {
-         res.status(400).send(error.message)
+        res.status(400).send(error)
     }
 })
 
@@ -35,19 +23,59 @@ app.get('/users', async (req, res) => {
 
     try {
         const users = await User.find({})
-         res.status(200).send(users)
+        res.status(200).send(users)
     } catch (error) {
-         res.status(500).send()
+        res.status(500).send()
     }
-
 })
+
+
+app.get('/users/:id', async ({ params }, res) => {
+    const _id = params.id
+
+    try {
+        const user = await User.findById(_id)
+        if (!user) {
+            return res.status(404).send('Could not find a user with ID: ' + _id)
+        }
+        res.status(200).send(user)
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+})
+
+app.patch('/users/:id', async ({ params, body }, res) => {
+    const _id = params.id
+
+    try {
+        const user = await User.findByIdAndUpdate(_id, body, { new: true, runValidators: true })
+        if (!user) {
+            return res.status(404).send('Could not find a user with ID: ' + _id)
+        }
+        res.status(200).send(user)
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+})
+
+app.post('/tasks', async ({ body }, res) => {
+    const task = new Task(body)
+
+    try {
+        await task.save()
+        res.status(201).send('New Task saved with name ' + task.name)
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
+})
+
 
 app.get('/tasks', async (req, res) => {
     try {
         const tasks = await Task.find({})
-         res.status(200).send(tasks)
+        res.status(200).send(tasks)
     } catch (error) {
-         res.status(500).send()
+        res.status(500).send()
     }
 
 })
@@ -61,26 +89,28 @@ app.get('/tasks/:id', async ({ params }, res) => {
         if (!task) {
             return res.status(404).send('Could not find a task with ID: ' + _id)
         }
-         res.status(200).send(task)
+        res.status(200).send(task)
     } catch (error) {
-         res.status(500).send(error.message)
+        res.status(500).send(error.message)
     }
 })
 
-
-app.get('/users/:id', async ({ params }, res) => {
+app.patch('/tasks/:id', async ({ params, body }, res) => {
     const _id = params.id
 
     try {
-        const user = await User.findById(_id)
-        if (!user) {
-            return res.status(404).send('Could not find a user with ID: ' + _id)
+        const task = await Task.findByIdAndUpdate(_id, body, { new: true, runValidators: true })
+        if (!task) {
+            return res.status(404).send('Could not find a task with ID: ' + _id)
         }
-         res.status(200).send(user)
+        res.status(200).send(task)
     } catch (error) {
-         res.status(500).send(error.message)
+        res.status(500).send(error.message)
     }
 })
+
+
+
 
 app.listen(port, () => {
     console.log('Server is running on ' + port)
